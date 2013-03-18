d3da729321cbcb6b59a4e7c4c24bcb3fd16992e3
Colin T.A. Gray <colinta@gmail.com>
2013-03-18 09:26:33 -0600

    update example

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,33 +3,17 @@ include SugarCube::Adjust
 
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window = Kiln::Window.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     ctlr = MyController.new
     first = UINavigationController.alloc.initWithRootViewController(ctlr)
     @window.rootViewController = first
     @window.makeKeyAndVisible
 
-    0.2.seconds.later { Kiln.fire_up }
     true
   end
 end
 
 
 class MyController < UITableViewController
-
-  def viewWillAppear(animated)
-    super
-    self.becomeFirstResponder
-  end
-
-  def canBecomeFirstResponder
-    true
-  end
-
-  def motionEnded(motion, withEvent:event)
-    if event.type == UIEventSubtypeMotionShake
-      Kiln.toggle
-    end
-  end
 
 end
