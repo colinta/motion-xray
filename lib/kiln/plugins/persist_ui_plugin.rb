6bbfa67d6a98149ba40043824f5fa97022a3a333
Colin T.A. Gray <colinta@gmail.com>
2013-03-18 09:14:40 -0600

    persist logs changes, still doesn't save them

--- lib/kiln/plugins/persist_ui_plugin.rb
+++ lib/kiln/plugins/persist_ui_plugin.rb
@@ -3,21 +3,37 @@ module Kiln
   class PersistUIPlugin < Plugin
     name 'Save UI'
 
+    def initialize
+      # uiview instance => list of changes
+      @changes = {}
+    end
+
     def kiln_view_in(canvas)
     end
 
     def save_changes(notification)
-      # notification.object
-      # notification.userInfo['property']
-      # notification.userInfo['value']
+      @changes[@editing] ||= {}
+      property = notification.userInfo['property']
+      value = notification.userInfo['value']
+      original = notification.userInfo['original']
+
+      if value == original
+        @changes[@editing].delete(property)
+      else
+        @changes[@editing][property] = notification.userInfo['value']
+      end
+
       NSLog("=============== persist_ui_plugin.rb line #{__LINE__} ===============
 =============== #{self.class == Class ? self.name + '##' : self.class.name + '#'}#{__method__} ===============
 notification.object: #{notification.object.inspect}
-notification.userInfo['property']: #{notification.userInfo['property'].inspect}
-notification.userInfo['value']: #{notification.userInfo['value'].inspect}")
+property: #{property.inspect}
+original: #{original.inspect}
+value: #{value.inspect}
+")
     end
 
     def kiln_edit(editing)
+      @editing = editing
       KilnNotificationTargetDidChange.remove_observer(self)
       KilnNotificationTargetDidChange.add_observer(self, :'save_changes:', @editing)
     end
